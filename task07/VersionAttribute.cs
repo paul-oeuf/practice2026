d5519161d15350e1a34405c0e9a0f5292f81ffed
paul-oeuf <paul.oeuf.bee@gmail.com>
2026-07-15 13:54:39 +0600

    Complete task07

---
+++ task07/VersionAttribute.cs
@@ -0,0 +1,15 @@
+namespace task07;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class VersionAttribute : Attribute
+{
+    public int Major { get; }
+
+    public int Minor { get; }
+
+    public VersionAttribute(int major, int minor)
+    {
+        Major = major;
+        Minor = minor;
+    }
+}
