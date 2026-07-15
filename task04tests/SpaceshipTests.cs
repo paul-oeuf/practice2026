cfa8932becb8de07ae1a6755647bb0df5ae9011c
paul-oeuf <paul.oeuf.bee@gmail.com>
2026-07-15 06:32:10 +0600

    Improve task04 tests

--- task04tests/SpaceshipTests.cs
+++ task04tests/SpaceshipTests.cs
@@ -5,52 +5,89 @@ namespace task04tests;
 
 public class SpaceshipTests
 {
-
     [Fact]
-    public void Cruiser_ShouldHaveCorrectStats()
+    public void Cruiser_ShouldHaveCorrectProperties()
     {
+        // Arrange
         ISpaceship cruiser = new Cruiser();
 
+        // Assert
         Assert.Equal(50, cruiser.Speed);
-
         Assert.Equal(100, cruiser.FirePower);
     }
 
-
     [Fact]
-    public void Fighter_ShouldHaveCorrectStats()
+    public void Fighter_ShouldHaveCorrectProperties()
     {
+        // Arrange
         ISpaceship fighter = new Fighter();
 
+        // Assert
         Assert.Equal(100, fighter.Speed);
-
         Assert.Equal(50, fighter.FirePower);
     }
 
-
     [Fact]
-    public void Ships_ShouldImplementInterface()
+    public void Cruiser_ShouldImplementInterface()
     {
-        ISpaceship cruiser = new Cruiser();
-        ISpaceship fighter = new Fighter();
-
+        var cruiser = new Cruiser();
 
         Assert.IsAssignableFrom<ISpaceship>(cruiser);
+    }
+
+    [Fact]
+    public void Fighter_ShouldImplementInterface()
+    {
+        var fighter = new Fighter();
 
         Assert.IsAssignableFrom<ISpaceship>(fighter);
     }
 
+    [Fact]
+    public void Cruiser_Methods_ShouldNotThrowExceptions()
+    {
+        // Arrange
+        ISpaceship cruiser = new Cruiser();
+
+        // Act
+        var exception = Record.Exception(() =>
+        {
+            cruiser.MoveForward();
+            cruiser.Rotate(90);
+            cruiser.Fire();
+        });
+
+        // Assert
+        Assert.Null(exception);
+    }
 
     [Fact]
-    public void ShipMethods_ShouldExecute()
+    public void Fighter_Methods_ShouldNotThrowExceptions()
     {
-        ISpaceship ship = new Fighter();
+        // Arrange
+        ISpaceship fighter = new Fighter();
 
+        // Act
+        var exception = Record.Exception(() =>
+        {
+            fighter.MoveForward();
+            fighter.Rotate(180);
+            fighter.Fire();
+        });
 
-        ship.MoveForward();
+        // Assert
+        Assert.Null(exception);
+    }
 
-        ship.Rotate(90);
+    [Fact]
+    public void DifferentShips_ShouldHaveDifferentCharacteristics()
+    {
+        // Arrange
+        ISpaceship cruiser = new Cruiser();
+        ISpaceship fighter = new Fighter();
 
-        ship.Fire();
+        // Assert
+        Assert.NotEqual(cruiser.Speed, fighter.Speed);
+        Assert.NotEqual(cruiser.FirePower, fighter.FirePower);
     }
 }
